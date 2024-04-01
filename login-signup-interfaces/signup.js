f3ce897fe1bf619c7537c9c0227f5945204225d1
nactuz <nactus123@gmail.com>
2024-04-02 02:40:15 +0530

    Add a button below Adhaar upload buttons, it will be visible once image is uploaded successfully, on clicking the uploaded image will be shown

--- login-signup-interfaces/signup.js
+++ login-signup-interfaces/signup.js
@@ -5,6 +5,8 @@ import { theme } from '../theme';
 import { useNavigation } from '@react-navigation/native';
 import { Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { Modal } from 'react-native';
+import { Pressable } from 'react-native';
 
 export default function LoginPage() {
     const navigation = useNavigation();
@@ -13,10 +15,8 @@ export default function LoginPage() {
     const [userType, setUserType] = useState('Student'); // Default user type
     const [aadharFront, setAadharFront] = useState(null);
     const [aadharBack, setAadharBack] = useState(null);
-
-    const handleShowImages = () => {
-        navigation.navigate('UploadedImagesScreen', { aadharFront, aadharBack });
-    }; 
+    const [frontModalVisible, setFrontModalVisible] = useState(false);
+    const [backModalVisible, setBackModalVisible] = useState(false);    
 
     const MAX_IMAGE_SIZE_BYTES = 500 * 1024; // 500 KB in bytes
 
@@ -27,9 +27,9 @@ export default function LoginPage() {
             aspect: [4, 3],
             quality: 1,
         });
-    
+
         console.log(result);
-    
+
         if (!result.cancelled) {
             const uri = result.assets[0].uri;
             const isSizeValid = await checkImageSize(uri); // Check image size
@@ -40,7 +40,7 @@ export default function LoginPage() {
             }
         }
     };
-    
+
     const handleAadharBackUpload = async () => {
         let result = await ImagePicker.launchImageLibraryAsync({
             mediaTypes: ImagePicker.MediaTypeOptions.All,
@@ -48,9 +48,9 @@ export default function LoginPage() {
             aspect: [4, 3],
             quality: 1,
         });
-    
+
         console.log(result);
-    
+
         if (!result.cancelled) {
             const uri = result.assets[0].uri;
             const isSizeValid = await checkImageSize(uri); // Check image size
@@ -61,15 +61,15 @@ export default function LoginPage() {
             }
         }
     };
-    
+
     const checkImageSize = async (uri) => {
         const response = await fetch(uri);
         const blob = await response.blob();
         const imageSizeBytes = blob.size;
-    
+
         return imageSizeBytes <= MAX_IMAGE_SIZE_BYTES; // Return true if size is within limit, otherwise false
     };
-    
+
 
     const handleLogin = () => {
         // Handle login logic here
@@ -154,12 +154,73 @@ export default function LoginPage() {
                     <View style={[{ marginTop: 5 }]}>
                         <Button title="Upload Aadhaar Front Image" onPress={handleAadharFrontUpload} color={theme.colors.primary} />
                     </View>
-                    {aadharFront && <Image source={{ uri: aadharFront }} style={{ width: 200, height: 200 }} />}
+                    {/* {aadharFront ? <Image source={{ uri: aadharFront }} style={{ width: 200, height: 200 }} /> : null} */}
+                    {aadharFront ? (
+                        <View style={[{ marginTop: 2, marginBottom: 2 }]}>
+                            <Modal
+                                animationType="slide"
+                                transparent={true}
+                                visible={frontModalVisible}
+                                onRequestClose={() => {
+                                    Alert.alert('Modal has been closed.');
+                                    setFrontModalVisible(!frontModalVisible);
+                                }}>
+                                <View style={styles.centeredView}>
+                                    <View style={[{ borderRadius: 5, backgroundColor: '#fff', padding: 5 }]}>
+                                        <Image source={{ uri: aadharFront }} style={{ width: 200, height: 200, borderRadius: 5 }} />
+                                        <Pressable
+                                            color={theme.colors.primary}
+                                            style={[{ backgroundColor: theme.colors.primary }, { borderRadius: 5, borderColor: '#fff', padding: 5, marginTop: 5}]}
+                                            onPress={() => setFrontModalVisible(!frontModalVisible)}>
+                                            <Text style={[{backgroundColor: 'red', padding: 5, borderRadius: 5}, styles.textStyle]}>CLOSE</Text>
+                                        </Pressable>
+                                    </View>
+                                </View>
+                            </Modal>
+                            <Pressable
+                                color={theme.colors.primary}
+                                style={[{ backgroundColor: theme.colors.primary }, { borderWidth: 1, borderColor: '#fff', padding: 5 }]}
+                                onPress={() => setFrontModalVisible(true)}>
+                                <Text style={styles.textStyle}>View Uploaded Image - Adhaar Front</Text>
+                            </Pressable>
+                        </View>
+                    ) : null}
+
                     {/* Aadhar back image picker */}
                     <View style={[{ marginTop: 5, marginBottom: 5 }]}>
                         <Button title="Upload Aadhaar Back Image" onPress={handleAadharBackUpload} color={theme.colors.primary} />
                     </View>
-                    {aadharBack && <Image source={{ uri: aadharBack }} style={{ width: 200, height: 200 }} />}
+                    {/* {aadharBack && <Image source={{ uri: aadharBack }} style={{ width: 200, height: 200 }} />} */}
+                    {aadharBack ? (
+                        <View style={[{ marginTop: 2, marginBottom: 2 }]}>
+                            <Modal
+                                animationType="slide"
+                                transparent={true}
+                                visible={backModalVisible}
+                                onRequestClose={() => {
+                                    Alert.alert('Modal has been closed.');
+                                    setBackModalVisible(!backModalVisible);
+                                }}>
+                                <View style={styles.centeredView}>
+                                    <View style={[{ borderRadius: 5, backgroundColor: '#fff', padding: 5 }]}>
+                                        <Image source={{ uri: aadharBack }} style={{ width: 200, height: 200, borderRadius: 5 }} />
+                                        <Pressable
+                                            color={theme.colors.primary}
+                                            style={[{ backgroundColor: theme.colors.primary }, { borderRadius: 5, borderColor: '#fff', padding: 5, marginTop: 5}]}
+                                            onPress={() => setBackModalVisible(!backModalVisible)}>
+                                            <Text style={[{backgroundColor: 'red', padding: 5, borderRadius: 5}, styles.textStyle]}>CLOSE</Text>
+                                        </Pressable>
+                                    </View>
+                                </View>
+                            </Modal>
+                            <Pressable
+                                color={theme.colors.primary}
+                                style={[{ backgroundColor: theme.colors.primary }, { borderWidth: 1, borderColor: '#fff', padding: 5 }]}
+                                onPress={() => setBackModalVisible(true)}>
+                                <Text style={styles.textStyle}>View Uploaded Image - Adhaar Back</Text>
+                            </Pressable>
+                        </View>
+                    ) : null}
                     <Button title="Sign Up" onPress={handleLogin} color={theme.colors.tertiary} />
                     <View style={[{ marginTop: 5 }, { borderWidth: 2, borderColor: theme.colors.tertiary, borderRadius: 5 }]}>
                         <Button title="Login" onPress={() => navigation.navigate('Login')} color={theme.colors.primary} borderBottomColor={theme.colors.tertiary} />
@@ -176,6 +237,7 @@ export default function LoginPage() {
 }
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
         alignItems: 'center',
@@ -206,4 +268,46 @@ const styles = StyleSheet.create({
     userTypeText: {
         color: theme.colors.accent1,
     },
-});
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 35,
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+    },
+    buttonOpen: {
+        backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+        backgroundColor: '#2196F3',
+    },
+    textStyle: {
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+}
+);
